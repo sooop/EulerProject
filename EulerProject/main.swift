101df64c5c47cf147d86a2b1c219269012bfa853
snotice <soooprmx@icloud.com>
2014-11-25 23:04:21 +0900

    Initial Commit

---
+++ EulerProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EulerProject
+//
+//  Created by sooop on 2014. 11. 25..
+//  Copyright (c) 2014년 sooop. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
